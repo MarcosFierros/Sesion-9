1219f95ccad6bd68ec6bfb4e018d721eb93bd067
Marcos FIerros <is702908@iteso.mx>
2019-03-06 21:07:14 -0600

    tarea2 no sirve update

---
+++ Sesion9/app/src/main/java/com/iteso/tarea2/FragmentHome.java
@@ -0,0 +1,19 @@
+package com.iteso.tarea2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentHome extends Fragment {
+
+    public FragmentHome() {}
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+
+}
--- Tarea2/app/src/main/java/com/iteso/tarea2/FragmentHome.java
+++
@@ -1,6 +0,0 @@
-package com.iteso.tarea2;
-
-import android.support.v4.app.Fragment;
-
-class FragmentHome extends Fragment {
-}
